33e5c8a3d18a61b3f4b9fdcf260854ec0304c8a5
Andreas Stehling <as@picopay.de>
2013-08-15 13:14:51 +0200

    Finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Danke für Dein Interesse!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password,
+                                   :password_confirmation)
+    end
+
 end
