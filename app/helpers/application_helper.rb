c8a20582c687713af182d4797b8f9adaaa132bae
Andreas Stehling <as@picopay.de>
2014-05-08 20:08:10 +0200

    Change base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Comparo"
+    base_title = "Power.Check"
     if page_title.empty?
       base_title
     else
